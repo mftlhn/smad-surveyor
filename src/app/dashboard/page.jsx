d04bb1d8caffe0b5fce803cf0943856ce9a1d1fa
Miftakhussolikhin <miftakhussolikhin@gmail.com>
2025-01-05 16:33:19 +0700

    balik lagi

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -17,7 +17,7 @@ const DashboardPage = () => {
     }, []);
 
     useEffect( () => {
-        if(token === null) router.push('/');
+        if(token === null) return;
         if (!token) {
             router.push('/');
         }
